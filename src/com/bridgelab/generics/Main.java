460178c27ea77e23e1fe10a6e5c1582f90861c80
Nishit <nishitranjan41@gmail.com>
2022-09-02 17:57:59 +0530

    :usingfloat

--- src/com/bridgelab/generics/Main.java
+++ src/com/bridgelab/generics/Main.java
@@ -4,10 +4,6 @@ public class Main {
     public static void main(String[] args) {
         MaxmiumValue maxNum = new MaxmiumValue();
         System.out.println("Test case 1");
-        maxNum.getMaxValue(1, 3, 2);
-        System.out.println("test case 2");
-        maxNum.getMaxValue(22,28,4);
-        System.out.println("Test case 3");
-        maxNum.getMaxValue(55,60,45);
+        maxNum.getMaxValue(1.5F,3.0F, 2.5F);
     }
 }
--- src/com/bridgelab/generics/MaxmiumValue.java
+++ src/com/bridgelab/generics/MaxmiumValue.java
@@ -1,17 +1,17 @@
 package com.bridgelab.generics;
 
 public class MaxmiumValue {
-    void getMaxValue(Integer val1, Integer val2, Integer val3){
-        if((val1.compareTo(val2)) >=0 && (val1.compareTo(val3)) >=0)
+    void getMaxValue(float val1, float val2, float val3){
+        if((val1>(val2)) && ((val1>(val3))))
             System.out.println(val1+ " is the largest");
 
-        else if ((val2.compareTo(val1)) >=0 && (val2.compareTo(val3)) >=0) {
+        else if ((val2>(val1)) && (val2>(val3)) ) {
             System.out.println(val2+" is largest");
         }
         else{
             System.out.println(val3+" is largest");
 
         }
-
     }
+
 }
