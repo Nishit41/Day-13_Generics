760ad3cbec0fe57c26024599ec35335d2abd4baf
Nishit <nishitranjan41@gmail.com>
2022-09-02 18:19:09 +0530

    this is to find maximumvalueusingstring

--- src/com/bridgelab/generics/Main.java
+++ src/com/bridgelab/generics/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         MaxmiumValue maxNum = new MaxmiumValue();
         System.out.println("Test case 1");
-        maxNum.getMaxValue(1.5F,3.0F, 2.5F);
+        maxNum.getMaxValue("Nishit","Ranjan", "Nishit");
     }
 }
--- src/com/bridgelab/generics/MaxmiumValue.java
+++ src/com/bridgelab/generics/MaxmiumValue.java
@@ -1,17 +1,14 @@
 package com.bridgelab.generics;
 
 public class MaxmiumValue {
-    void getMaxValue(float val1, float val2, float val3){
-        if((val1>(val2)) && ((val1>(val3))))
+    void getMaxValue(String val1, String val2, String val3){
+        if( ((val1).compareTo(val2)>0) && ((val1).compareTo(val3)>0))
             System.out.println(val1+ " is the largest");
-
-        else if ((val2>(val1)) && (val2>(val3)) ) {
+        else if( ((val2).compareTo(val1)>0) && ((val2).compareTo(val3)>0)) {
             System.out.println(val2+" is largest");
         }
         else{
             System.out.println(val3+" is largest");
-
         }
     }
-
 }
