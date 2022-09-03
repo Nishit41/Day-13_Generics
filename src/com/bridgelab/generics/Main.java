32db1f3411671379edf827791149272146da183d
Nishit <nishitranjan41@gmail.com>
2022-09-03 16:57:57 +0530

    this is usin generic method

--- src/com/bridgelab/generics/Main.java
+++ src/com/bridgelab/generics/Main.java
@@ -5,5 +5,9 @@ public class Main {
         MaxmiumValue maxNum = new MaxmiumValue();
         System.out.println("Test case 1");
         maxNum.getMaxValue("Nishit","Ranjan", "Nishit");
+        System.out.println("test cae 2");
+        maxNum.getMaxValue(1.0f,5.0f,9.8f);
+        System.out.println("test case 3");
+        maxNum.getMaxValue(1,8,2);
     }
 }
